0ce855a57eeb08a91ed6fe5f93ca4245d21ddad3
Andrew Marquez <andy2mgcc@gmail.com>
2018-09-20 22:05:03 -0600

    Render a meme preview

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function renderMemeCanvas(memeImg) {
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+    canvas.width = memeImg.width;
+    canvas.height = memeImg.height;
+    context.drawImage(memeImg, 0, 0);
+}
+
 function validateImage(file) {
     var acceptableImages = ["image/gif", "image/jpeg", "image/png"];
     if (acceptableImages.includes(file.type)) {
@@ -6,7 +14,7 @@ function validateImage(file) {
     return false;
 }
 
-function updateImage() {
+function previewImage() {
     var warningText = document.getElementById('warning');
 
     if (originalInput.files && validateImage(originalInput.files[0])) {
@@ -15,6 +23,10 @@ function updateImage() {
         var reader = new FileReader();
         reader.onload = function(e) {
             preview.src = e.target.result;
+            renderMemeCanvas(preview)
+            originalInput.disabled = true;
+            warningText.style.color = 'green';
+            warningText.innerHTML = 'File upload success! If you would like to make another meme, refresh the page'
         }
 
         reader.readAsDataURL(imgFile);
@@ -24,13 +36,17 @@ function updateImage() {
 }
 
 function addText() {
-    console.log('you wrote: ' + memeText.value);
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "30px Arial";
+    context.strokeText(memeText.value, 10, 30);
 }
 
 window.onload = function() {
     var preview = document.getElementById('preview')
     var originalInput = document.getElementById('originalInput');
     var memeText = document.getElementById('memeText');
-    originalInput.addEventListener("change", updateImage);
+    originalInput.addEventListener("change", previewImage);
     memeText.addEventListener("change", addText)
 };
