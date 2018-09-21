67b29d99cb09991b3c66046bd4527bf9e492c373
Andrew Marquez <andy2mgcc@gmail.com>
2018-09-20 18:49:23 -0600

    Added file input and preview mechanism

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+function validateImage(file) {
+    var acceptableImages = ["image/gif", "image/jpeg", "image/png"];
+    if (acceptableImages.includes(file.type)) {
+        return true;
+    }
+    return false;
+}
+
+function updateImage() {
+    var warningText = document.getElementById('warning');
+
+    if (originalInput.files && validateImage(originalInput.files[0])) {
+        warningText.innerHTML = "";
+        var imgFile = originalInput.files[0];
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+        }
+
+        reader.readAsDataURL(imgFile);
+    } else {
+       warningText.innerHTML = 'Invalid file type.  Please choose a jpeg, png, or gif';
+    }
+}
+
+function addText() {
+    console.log('you wrote: ' + memeText.value);
+}
+
 window.onload = function() {
-    console.log('Initialized console')
+    var preview = document.getElementById('preview')
+    var originalInput = document.getElementById('originalInput');
+    var memeText = document.getElementById('memeText');
+    originalInput.addEventListener("change", updateImage);
+    memeText.addEventListener("change", addText)
 };
