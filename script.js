cdc1d41eeaba6f169762b67085e563cac50ca279
Andrew Marquez <andy2mgcc@gmail.com>
2018-09-20 23:37:25 -0600

    Added text drawing functionality

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
+function splitter(s) { // Credit for this function goes to Goran Andersson from Stack Overflow
+    var middle = Math.floor(s.length / 2);
+    var before = s.lastIndexOf(' ', middle);
+    var after = s.indexOf(' ', middle + 1);
+
+    if (before == -1 || (after != -1 && middle - before >= after - middle)) {
+        middle = after;
+    } else {
+        middle = before;
+    }
+
+    var s1 = s.substr(0, middle);
+    var s2 = s.substr(middle + 1);
+
+    return {s1, s2};
+}
+
 function renderMemeCanvas(memeImg) {
-    canvas = document.getElementById('canvas');
-    context = canvas.getContext('2d');
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
     canvas.width = memeImg.width;
     canvas.height = memeImg.height;
     context.drawImage(memeImg, 0, 0);
+    
+    // Add text, if there is any
+    words = splitter(memeText.value);
+    t1 = words["s1"]
+    t2 = words["s2"]
+    width = 30;
+    tooWide = true;
+    while (tooWide) {
+        context.font = width.toString() + "px " + "Sans-serif";
+        context.strokeStyle = "black";
+        context.lineWidth = 4;
+        t1Width = context.measureText(t1).width;
+        t2Width = context.measureText(t2).width;
+        tooWide = t1Width > canvas.width || t2Width > canvas.width;
+        if (tooWide) {
+            width -= 2;
+        }
+    }
+    
+    context.fillStyle = "white"
+    context.strokeText(t1, (canvas.width - t1Width) / 2, width);
+    context.fillText(t1, (canvas.width - t1Width) / 2, width);
+    context.strokeText(t2, (canvas.width - t2Width) / 2, canvas.height - width);
+    context.fillText(t2, (canvas.width - t2Width) / 2, canvas.height - width);
 }
 
 function validateImage(file) {
@@ -23,30 +64,21 @@ function previewImage() {
         var reader = new FileReader();
         reader.onload = function(e) {
             preview.src = e.target.result;
-            renderMemeCanvas(preview)
             originalInput.disabled = true;
             warningText.style.color = 'green';
-            warningText.innerHTML = 'File upload success! If you would like to make another meme, refresh the page'
+            warningText.innerHTML = 'File upload successful! If you would like to make another meme, refresh the page'
+            renderMemeCanvas(preview);
         }
-
         reader.readAsDataURL(imgFile);
     } else {
        warningText.innerHTML = 'Invalid file type.  Please choose a jpeg, png, or gif';
     }
 }
 
-function addText() {
-    canvas = document.getElementById('canvas');
-    context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.font = "30px Arial";
-    context.strokeText(memeText.value, 10, 30);
-}
-
 window.onload = function() {
-    var preview = document.getElementById('preview')
+    var preview = document.getElementById('preview');
     var originalInput = document.getElementById('originalInput');
     var memeText = document.getElementById('memeText');
     originalInput.addEventListener("change", previewImage);
-    memeText.addEventListener("change", addText)
+    memeText.addEventListener("change", previewImage);
 };
